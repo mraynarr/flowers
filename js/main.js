f09319963465f149662fe0222df178c6472b4041
mraynarr <raynarham23@gmail.com>
2024-09-29 14:50:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I DONT CARE').split('')
+    const titles = ('AYO FOTO CUY').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
