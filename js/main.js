93ee6cf571916381550687bf5a9a6a61c5c1c4a8
mraynarr <raynarham23@gmail.com>
2024-09-30 08:40:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('AYO FOTO CUY').split('')
+    const titles = ('AYO FOTO CUY PAKE BATIK').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
